1b606ac5ba2e5801a604144c5744282220109b7e
Padma Patil <padma@email.com>
2018-08-21 23:17:35 -0700

    Backend and frontend complete in scripts file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
+//frontendLogic
 $(document).ready(function() {
   $("#beepBoop").submit(function(event) {
-    debugger;
     event.preventDefault();
     var inputNumber = parseInt($("#number").val());
+//BackendLogic
     var result=[];
-    for(index=0;index<=inputNumber;index++) {
-        if(index===0) {
-          result.push("Beep")
-        }else if(index===1){
-          result.push("Boop")
-        }else if(index%3===0){
+    for(var index=0;index<=inputNumber;index++) {
+      if((index.toString().includes("1"))||(index.toString().includes("0"))||(index%3===0))
+      {
+        if(index%3===0) {
           result.push("I'm sorry, Dave. I'm afraid I can't do that.")
-        }else{
-          result.push(index)
+        }else if(index.toString().includes("1")){
+          result.push("Boop");
+        }else if(index.toString().includes("0")){
+          result.push("Beep");
         }
-    }
+      }else{
+          result.push(index);
+        }
+      }
+//frontendLogic
     $(".output").append("<p>" + result + "</p>")
-
   });
 });
